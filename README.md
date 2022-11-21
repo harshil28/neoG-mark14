d0b817e88cfc67c93c8669a9cf6abbb5138c663c
Harshil <harshildilse2000@gmail.com>
2022-11-21 21:55:07 +0530

    fix: Now negative values show proer Error

---
+++ README.md
@@ -0,0 +1,5 @@
+# neoG mark14 - Stocks Profit & Loss
+
+Created a simple app in which profit/loss percentage and total amount of STOCKS can be known upon entering their Initial Price, Current Price and Quantity purchased.
+
+Apun Chaand par jaayega ab..!!
--- index.js
+++ index.js
@@ -5,35 +5,54 @@ var showButton = document.querySelector("#showButton");
 var finalOutput = document.querySelector("#output");
 
 function showMessage(message) {
+  finalOutput.style.display = "block";
   finalOutput.innerText = message;
 }
 
+function showErrorMessage(message) {
+  finalOutput.style.display = "block";
+  finalOutput.style.color = "red";
+  finalOutput.innerText = message;
+}
+
+function hideMessage() {
+  finalOutput.style.display = "none";
+}
+
 function calculateProfitAndLoss() {
+
 
 var initialPrice = ip.value;
 var quantity = qt.value;
 var currentPrice = cp.value;
 
-
-  if (initialPrice < currentPrice) {
-    // console.log(currentPrice);
-    // console.log(initialPrice);
-    var profit =
-      (currentPrice - initialPrice) * quantity;
-    var profitPercentage = (((currentPrice - initialPrice) / initialPrice) * 100).toFixed(2);
-    showMessage("Your Profit is " + profit + " & Profit Percentage is " + profitPercentage + " %");
-  }else if (initialPrice > currentPrice) {
-    // console.log(currentPrice<initialPrice);
-    // console.log(currentPrice);
-    // console.log(initialPrice);
-    // console.log("also working");
-    var loss =
-      (initialPrice - currentPrice) * quantity;
-    var lossPercentage = (((initialPrice - currentPrice) / initialPrice) * 100).toFixed(2);
-    showMessage("Your Loss is " + loss + " & Loss Percentage is " + lossPercentage + " %");
-  }else if (currentPrice == initialPrice){
-    showMessage("No Pain,No Gain!!");
-  }
+hideMessage();
+if(initialPrice>0){
+    if(quantity>0)
+    {
+        if(currentPrice>=0){
+            if (initialPrice < currentPrice) {
+                var profit =
+                  (currentPrice - initialPrice) * quantity;
+                var profitPercentage = (((currentPrice - initialPrice) / initialPrice) * 100).toFixed(2);
+                showMessage("Your Profit is " + profit + " & Profit Percentage is " + profitPercentage + " %");
+              }else if (initialPrice > currentPrice) {
+                var loss =
+                  (initialPrice - currentPrice) * quantity;
+                var lossPercentage = (((initialPrice - currentPrice) / initialPrice) * 100).toFixed(2);
+                showMessage("Your Loss is " + loss + " & Loss Percentage is " + lossPercentage + " %");
+              }else if (currentPrice == initialPrice){
+                showMessage("No Pain,No Gain!!");
+              }
+        }else{
+        showErrorMessage("Enter positive current value!!");
+        }
+    }else{
+    showErrorMessage("Enter positive quantity!!");
+    }
+}else{
+    showErrorMessage("Enter positive initial value!!");
+}
 }
 
 showButton.addEventListener('click',calculateProfitAndLoss);
